835ef5bc03978398d5f26dfd30fb97f468797ced
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-02-04 22:40:17 +0800

    added login & register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SocialLogin from "../../components/SocialLogin/SocialLogin";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="w-full m-4 max-w-md mx-auto p-4 rounded-md shadow sm:p-8 bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
       <h2 className="mb-3 text-3xl font-semibold text-center">
-        Login to your account
+        Sign up to your account
       </h2>
       <p className="text-sm text-center text-gray-400 dark:text-gray-600">
-        Don't have account?
+        Already have account?
         <Link
-          to="/register"
+          to="/login"
           href="#"
           rel="noopener noreferrer"
           className="focus:underline hover:underline text-primary"
         >
-          Sign up here
+          Login here
         </Link>
       </p>
       {/* Social Login */}
@@ -28,6 +28,30 @@ const Login = () => {
       </div>
       <form noValidate="" action="" className="space-y-8">
         <div className="space-y-4">
+          <div className="space-y-2">
+            <label htmlFor="name" className="block text-sm">
+              Name
+            </label>
+            <input
+              type="name"
+              name="name"
+              id="name"
+              placeholder="Your Name"
+              className="w-full px-3 py-2 border rounded-md border-gray-700 dark:border-gray-300 bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800 focus:border-violet-400 focus:dark:border-violet-600"
+            />
+          </div>
+          <div className="space-y-2">
+            <label htmlFor="imageURL" className="block text-sm">
+            PhotoURL
+            </label>
+            <input
+              type="text"
+              name="image"
+              id="name"
+              placeholder="Photo URL"
+              className="w-full px-3 py-2 border rounded-md border-gray-700 dark:border-gray-300 bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800 focus:border-violet-400 focus:dark:border-violet-600"
+            />
+          </div>
           <div className="space-y-2">
             <label htmlFor="email" className="block text-sm">
               Email address
@@ -45,13 +69,7 @@ const Login = () => {
               <label htmlFor="password" className="text-sm">
                 Password
               </label>
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs hover:underline text-gray-400 dark:text-gray-600"
-              >
-                Forgot password?
-              </a>
+             
             </div>
             <input
               type="password"
@@ -63,14 +81,14 @@ const Login = () => {
           </div>
         </div>
         <button
-          type="button"
+          type="submit"
           className="w-full px-8 py-3 font-semibold rounded-md bg-violet-400 dark:bg-violet-600 text-gray-900 dark:text-gray-50"
         >
-          Sign in
+          Sign Up
         </button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Register;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,36 @@
-import { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useState } from "react";
+import app from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-const authInfo = {};
-
 const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+
+  // for new user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //  log in user
+  const signIn = (email, password) => {
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   log Out user
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const authInfo = { createUser, signIn, logOut, loading, setLoading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
